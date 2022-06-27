73c601408ea1823c328b6733be3eab7e69fb9433
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2022-06-26 20:55:03 -0400

    define Map trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+trait Map<K, V> {
+    fn get(key: &K) -> Option<V>;
+    fn contains(key: &K) -> bool;
+    fn put(key: K, value: V);
+    fn remove(key: &K) -> Option<V>;
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
