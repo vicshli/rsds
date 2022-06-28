08a8d58e231a556fa51c7dadd21eab9bea871f5c
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2022-06-27 20:59:33 -0400

    streamline bench code

--- bench/bench_striped_map.rs
+++ bench/bench_striped_map.rs
@@ -5,6 +5,15 @@ use rand::{distributions::Alphanumeric, Rng};
 use rsds::{Map, StripedHashMap};
 use std::time::Instant;
 
+macro_rules! bench {
+    ($name: expr, $body: expr) => {
+        let now = Instant::now();
+        $body;
+        let elapsed = now.elapsed();
+        println!("{} elapsed: {:.2?}", $name, elapsed);
+    };
+}
+
 fn make_random_string() -> String {
     rand::thread_rng()
         .sample_iter(&Alphanumeric)
@@ -19,28 +28,25 @@ fn make_random_string_pairs(n: usize) -> Vec<(String, String)> {
         .collect()
 }
 
-fn bench_single_threaded<K: Hash + PartialEq + Eq + Clone, V: Clone>(src: Vec<(K, V)>) {
+fn bench_single_threaded<K: Hash + PartialEq + Eq + Clone, V: Clone>(src: &Vec<(K, V)>) {
+    let map_data = src.clone();
+    bench!("StripedHashMap", {
+        let map = StripedHashMap::new();
+        for (key, val) in map_data {
+            map.put(key, val);
+        }
+    });
+
     let dmap_data = src.clone();
-
-    let now = Instant::now();
-    let map = StripedHashMap::new();
-    for (key, val) in src {
-        map.put(key, val);
-    }
-
-    let elapsed = now.elapsed();
-    println!("StripedHashMap Elapsed: {:.2?}", elapsed);
-
-    let now = Instant::now();
-    let dmap = DashMap::new();
-    for (key, val) in dmap_data {
-        dmap.insert(key, val);
-    }
-    let elapsed = now.elapsed();
-    println!("DashMap Elapsed: {:.2?}", elapsed);
+    bench!("DashMap", {
+        let map = DashMap::new();
+        for (key, val) in dmap_data {
+            map.insert(key, val);
+        }
+    });
 }
 
 fn main() {
     let input = make_random_string_pairs(1_000_000);
-    bench_single_threaded(input);
+    bench_single_threaded(&input);
 }
